10427edb973b2471caeb4066aafc6181d143af23
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-05-19 10:56:36 +0200

    chore: adding wrapper to loading content

--- apps/web/app/dashboard/loading.tsx
+++ apps/web/app/dashboard/loading.tsx
@@ -4,13 +4,15 @@ import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function Loading() {
   return (
-    <div className={styles.loader}>
-      <Skeleton
-        count={20}
-        baseColor="#1f252d"
-        highlightColor="#383838"
-        height={80}
-      />
-    </div>
+    <main className={styles.main}>
+      <div className={styles.loader}>
+        <Skeleton
+          count={20}
+          baseColor="#1f252d"
+          highlightColor="#383838"
+          height={80}
+        />
+      </div>
+    </main>
   )
 }
